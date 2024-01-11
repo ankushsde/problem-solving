0372e4dd6c93e066d54fa484294801b48f9936f5
singhanku815@gmail.com <Ankush>
2024-01-11 10:07:36 +0530

    timeout and closure

--- 2024/Javascript/closure.js
+++ 2024/Javascript/closure.js
@@ -4,13 +4,26 @@ function a(){
         console.log(x)
     }
     b()
-
 }
 
-let z = a( );
+// let z = a( );
 
 //a function that bind with its lexical enviornment.
 // lexical enviornment : whenever an execution context is created lexical enviornment is also created.
 // lexical enviornment is the local memory along with the enviornment of its parent.
  
-  
+
+let printValue = () => {
+    for(var i = 1;  i <=5;i++){
+      function inside(x){
+        setTimeout(function(){
+            console.log(x);
+        }, x * 1000)
+      }
+      inside(i)
+    }
+}
+
+
+printValue()
+
