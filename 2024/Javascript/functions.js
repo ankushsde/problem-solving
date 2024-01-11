1ea42b840bf4304fe59fc2152c02678272fab6a8
singhanku815@gmail.com <Ankush>
2024-01-11 11:21:57 +0530

    functions

---
+++ 2024/Javascript/functions.js
@@ -0,0 +1,30 @@
+
+
+// function statement
+
+function a(){
+    console.log("a is called")
+}
+
+//function Expression
+
+var b = function (){
+    console.log(" b is called")
+}
+
+// Function Declaration : This is similiar to function statement 
+
+//Anonyms functions
+
+// function () {
+
+// }    these functions are used in a place where functions are used as values.
+
+
+//named Function Expressions
+
+var c = function xyz(){
+console.log(xyz)
+}
+// c()
+
