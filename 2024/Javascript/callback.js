950517eb1470aed535a6cc2deef5d8101357f3b6
ankushsde <ankushs5252@gmail.com>
2024-01-20 11:23:13 +0530

    callback, eventloop

--- 2024/Javascript/callback.js
+++ 2024/Javascript/callback.js
@@ -1,15 +1,35 @@
-function sendMessage (message){
-    console.log(message)
-}
+// function sendMessage (message){
+//     console.log(message)
+// }
 
-function getMessage(sendMessage){
-    const message = "This is a secret message"
-    sendMessage(message)
-} 
+// function getMessage(sendMessage){
+//     const message = "This is a secret message"
+//     sendMessage(message)
+// } 
 
-getMessage(sendMessage)
+// getMessage(sendMessage)
 
 
 //A callback function is a function that is passed as an argument to another function.
 //and it is executed after some operation/event is executed.
+
+// setTimeout(function(){
+//  console.log("timer")
+// },5000)
+
+// function x(y){
+//     console.log("x")
+//     y();
+// }
+
+// x(function y (){
+//     console.log("y")
+// })
+
+
+
+document.getElementById("clickMe").addEventListener("click",function xyz(){
+ console.log("Button Clicked")
+})
+
 
