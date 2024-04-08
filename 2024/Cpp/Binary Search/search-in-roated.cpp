44799abbbc2ce793f30b575907d1cf390249afcf
ankushsde <ankushs5252@gmail.com>
2024-04-08 14:43:17 +0530

    April 8 , Binary Search

---
+++ 2024/Cpp/Binary Search/search-in-roated.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        vector<int> arr =nums;
+        int k = target;
+        int low = 0; 
+        int high = nums.size()-1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return mid;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+    }
+};
+
+//TC: O(logn) SC: O(1)
