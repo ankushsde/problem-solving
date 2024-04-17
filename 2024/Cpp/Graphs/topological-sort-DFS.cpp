0280144be9a5496eee6dd1b44429f0447bdadfbc
ankushsde <ankushs5252@gmail.com>
2024-04-17 17:00:28 +0530

    Apr 17 , Graph II

--- 2024/Cpp/Graphs/topological-sort-DFS.cpp
+++ 2024/Cpp/Graphs/topological-sort-DFS.cpp
@@ -6,26 +6,26 @@ class Solution {
 private: 
     void dfs(int node, int vis[], vector<int> &st, vector<int> adj[]) {
         vis[node] = 1;
-        for(auto it : adj[node]) { // 1. Iterate over adjacent nodes of 'node'
+        for(auto it : adj[node]) { 
             if(!vis[it]) {
-                dfs(it, vis, st, adj); // 2. Pass 'it' instead of 'node'
+                dfs(it, vis, st, adj);  
             }
         }
-        st.push(node); // 3. Push 'node' to stack 'st'
+        st.push(node);  
     }
 
 public: 
     vector<int> topoSort(int V, vector<int> adj[]) {
-        int* vis = new int[V]; // 5. Declare 'vis' using 'new'
+        int* vis = new int[V];  
         vector<int> st;
         
         for(int i = 0; i < V; i++) {
             if(!vis[i]) {
-                dfs(i, vis, st, adj); // 4. Add missing semicolon
+                dfs(i, vis, st, adj);  
             }
         }
 
-        delete[] vis; // Free the memory allocated for 'vis'
+        delete[] vis;  
 
         vector<int> ans;
         while(!st.empty()) {
