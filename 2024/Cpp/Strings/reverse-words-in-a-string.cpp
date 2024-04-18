476ed736980fd5273131233f91a6e0649a45f8b7
ankushsde <ankushs5252@gmail.com>
2024-04-18 21:06:47 +0530

    Aprl 18, algos and strings

---
+++ 2024/Cpp/Strings/reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+// approach: 
+//reverse the string,
+//reverse individual words while handling the extra spaces 
+// removing the extra spaces and returning
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int left=0;
+        int right=0;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' ')i++;
+            if(i==n)break; // to stop index going out of bounds
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+            }
+            reverse(s.begin()+left,s.begin()+right);
+            s[right++]=' ';
+            left=right;
+            i++;
+        }
+        s.resize(right-1);
+        return s;
+    }
+};
