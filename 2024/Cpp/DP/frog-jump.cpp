eec2518722933f0ef0fbc2226364f96002633389
ankushsde <ankushs5252@gmail.com>
2024-04-22 11:18:16 +0530

    additional to frog jump

--- 2024/Cpp/DP/frog-jump.cpp
+++ 2024/Cpp/DP/frog-jump.cpp
@@ -55,5 +55,20 @@ int frogJump(int n, vector<int> &heights)
 
 //uisng Tabulation - Optimized space, playing around the indexes, prev, prev2, and current i
 
-
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp (n,0);
+    // dp[0] = 0;
+    prev = 0;
+    prev2 = 0;
+    for(int i = 1; i<n;i++){
+     int fs = prev + abs(heights[i]-heights[i-1]);
+     int ss = INT_MAX;
+     if(i>1) ss = prev2 + abs(heights[i] - heights[i-2]);    
+     int curi = min(fs,ss);
+     prev2 = prev;
+     prev = curi; 
+    }
+    return prev;
+}
 
