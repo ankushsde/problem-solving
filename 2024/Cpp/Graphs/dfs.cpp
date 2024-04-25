0075536d2262f3cb0d55ed9e34d38081048715bc
ankushsde <ankushs5252@gmail.com>
2024-04-25 17:35:18 +0530

    Update Apr 25

--- 2024/Cpp/Graphs/dfs.cpp
+++ 2024/Cpp/Graphs/dfs.cpp
@@ -6,7 +6,7 @@ class Solution {
 
 private : 
 
-void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls){
+void dfs(int node, vector<int> &adj[], int vis[], vector<int> &ls){
  
  vis[node] = 1;
  ls.push_back(node);
