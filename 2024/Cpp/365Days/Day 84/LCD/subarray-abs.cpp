6d718d8b94ad4c23c684dec090f03c6c21308331
ankushsde <ankushs5252@gmail.com>
2024-06-23 19:59:38 +0530

    Day : 84 , Arrays, LCD , Algodaily

---
+++ 2024/Cpp/365Days/Day 84/LCD/subarray-abs.cpp
@@ -0,0 +1,40 @@
+// this problem is solved using deque and sliding window pattern 
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+      deque<int> increase;
+      deque<int> decrease;
+
+      int maxLen = 0; 
+      int left = 0;
+
+      for(int right = 0; right<nums.size(); right++ ){
+
+        while(!increase.empty() && nums[right]<increase.back()){
+            increase.pop_back();
+        }
+        increase.push_back(nums[right]);
+
+        while(!decrease.empty() && nums[right]>decrease.back()){
+            decrease.pop_back();
+        }
+
+        decrease.push_back(nums[right]);
+
+        while(decrease.front()-increase.front() > limit){
+            if(nums[left] == decrease.front()){
+                decrease.pop_front();
+            }
+            if(nums[left] == increase.front()){
+                increase.pop_front();
+            }
+            ++left;
+        }
+        maxLen = max(maxLen, right-left+1);
+      }
+      return maxLen;
+    }
+};
+
+// TC: O(n)
+// SC: O(n)
